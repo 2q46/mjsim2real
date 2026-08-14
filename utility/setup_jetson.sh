1a5ac5baa8dcf0c3807436eb5e8a3e4576022791
Ishan Agrawal <i1agrawal@ucsd.edu>
2026-08-14 01:04:37 -0700

    setup jetson

--- utility/setup_jetson.sh
+++ utility/setup_jetson.sh
@@ -1,3 +1,9 @@
-pip install mujoco
-pip install mujoco-warp
-pip install warp-lang
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+# 2. Clear the broken index URL environment variable
+unset PIP_INDEX_URL
+unset PIP_EXTRA_INDEX_URL
+
+# 3. Install MuJoCo as a single single-line command
+pip install mujoco mujoco-warp warp-lang optax distrax --index-url https://pypi.org/simple
