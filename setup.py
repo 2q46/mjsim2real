bbdf00cf6b17803e81017eb65a1800320bac757a
Ishan Agrawal <i1agrawal@ucsd.edu>
2026-08-16 23:28:26 -0700

    setup tools

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    
+    name="pickcube-mjsim2real-rl",
+    version="0.1.0",
+    packages=find_packages(
+        include=["rl", "rl.*", "env", "env.*", "utility", "utility.*"]
+    ),
+    python_requires="==3.11.*",
+    install_requires=[
+        "mediapy",
+        "wandb",
+        "mujoco",
+        "warp-lang",
+        "mujoco-warp",
+        "flax",
+        "optax",
+        "distrax"
+    ]
 )
